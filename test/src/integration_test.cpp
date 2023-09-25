c6ac057bafef91919064e0f1003c1f449289e44f
iwishiwasaneagle <jh.ewers@gmail.com>
2023-09-25 17:16:17 +0100

    test: Reduce redundant tests by using parameterization

--- test/src/integration_test.cpp
+++ test/src/integration_test.cpp
@@ -1,68 +1,86 @@
 // #  Copyright 2023 Jan-Hendrik Ewers
 // #  SPDX-License-Identifier: GPL-3.0-only
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
-#include <jpathgen/integration.h>
 #include <jpathgen/environment.h>
 #include <jpathgen/function.h>
-#include <jpathgen/geometry.h>
+#include <jpathgen/integration.h>
 
 #include <Eigen/Core>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <map>
 
 using namespace jpathgen::integration;
 using namespace jpathgen::function;
 using namespace jpathgen::environment;
 using namespace jpathgen::geometry;
+using Catch::Matchers::WithinRel;
 
 double fn1(double a, double b){
   return 1;
 }
 
-SCENARIO("The integration function is called with the various allowed types", "[integration]"){
-  GIVEN("A function::Function"){
-    WHEN("And a Eigen::Matrix of coords is passed"){
-      EigenCoords coords = Eigen::Matrix<double, 10,2>::Random();
-      THEN("A double is returned without error"){
-        REQUIRE(integrate_over_buffered_line(fn1, coords) > -INFINITY);
-      }
-    }
-    WHEN("And a vector of coords is passed"){
-      STLCoords coords = {{0,0},{0,1},{10,10}};
-      THEN("A double is returned without error"){
-        REQUIRE(integrate_over_buffered_line(fn1, coords) > -INFINITY);
-      }
-    }
-    WHEN("A rectangle is passed"){
-      THEN("A double is returned without error"){
-        REQUIRE(integrate_over_rect(fn1, 0,10,0,10) > 0 );
-      }
+MultiModalBivariateGaussian generate_mmbg(int n_modes=1){
+  MUS mus = Eigen::Matrix<double, -1, 2>::Zero(n_modes,2);
+  COV cov = COV::Identity();
+  COVS covs = Eigen::Matrix<double, -1, 2>::Zero(n_modes*2, 2);
 
-      THEN("The area of the bounds is returned when the function always returns 1"){
-        double act = integrate_over_rect(fn1, 0,10,0,10);
-        REQUIRE(act == 100);
-      }
-    }
+  for (int i = 0; i < n_modes; ++i)
+  {
+    covs.block<2,2>(i*2, 0) = cov;
   }
-  GIVEN("A MultiModalBivariateGaussian"){
-    MU mu = MU::Zero();
-    COV cov = COV::Identity();
-    MultiModalBivariateGaussian mmbg(mu, cov);
+  MultiModalBivariateGaussian mmbg(mus, covs);
+  return mmbg;
+}
 
+enum CALLABLE{FUNCTION, MMBG};
+std::map<int, std::string> CALLABLE2STRING = {
+  {CALLABLE::FUNCTION, "function::Function"},
+  {CALLABLE::MMBG, "MultiModalBivariateGaussian"},
+};
+
+SCENARIO("The integration function is called with the various allowed types", "[integration]"){
+  int n_modes = GENERATE(1, 5, 10);
+  CALLABLE method = GENERATE(CALLABLE::FUNCTION, CALLABLE::MMBG);
+  Function fn;
+  switch (method)
+  {
+    case CALLABLE::FUNCTION:
+      fn = fn1;
+      break;
+    case CALLABLE::MMBG:
+      fn = std::bind(generate_mmbg(n_modes), std::placeholders::_1, std::placeholders::_2);
+      break;
+  }
+
+  DYNAMIC_SECTION("A " << CALLABLE2STRING[method]){
     WHEN("And a Eigen::Matrix of coords is passed"){
       EigenCoords coords = Eigen::Matrix<double, 10,2>::Random();
       THEN("A double is returned without error"){
-        REQUIRE(integrate_over_buffered_line(mmbg, coords) > -INFINITY);
+        REQUIRE(integrate_over_buffered_line(fn, coords) > -INFINITY);
       }
     }
     WHEN("And a vector of coords is passed"){
       STLCoords coords = {{0,0},{0,1},{10,10}};
       THEN("A double is returned without error"){
-        REQUIRE(integrate_over_buffered_line(mmbg, coords) > -INFINITY);
+        REQUIRE(integrate_over_buffered_line(fn, coords) > -INFINITY);
       }
     }
-    WHEN("A rectangle is passed"){
-      THEN("A double is returned without error"){
-        REQUIRE(integrate_over_rect(mmbg, 0,10,0,10) > 0 );
+    WHEN("And a rectangle is passed"){
+      double act = integrate_over_rect(fn, 0,10,0,10);
+      THEN("A non-zero value is returned"){
+        REQUIRE(act > 0 );
+      }
+      THEN("The value is as expected"){
+        switch (method)
+        {
+          case CALLABLE::FUNCTION:
+            REQUIRE(act == 10*10);
+            break;
+          case CALLABLE::MMBG:
+            REQUIRE_THAT(0.25, WithinRel(act, 1e-6));
+            break;
+        }
       }
     }
   }
