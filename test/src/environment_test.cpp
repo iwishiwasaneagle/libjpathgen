09936c5c3efb6bea68b25284bdec7eda52de275e
Jan-Hendrik Ewers <jh.ewers@gmail.com>
2023-09-25 18:41:51 +0100

    test: Reduce redundant tests by using parameterization (#4)

--- test/src/environment_test.cpp
+++ test/src/environment_test.cpp
@@ -1,114 +1,93 @@
 // #  Copyright 2023 Jan-Hendrik Ewers
 // #  SPDX-License-Identifier: GPL-3.0-only
 
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <jpathgen/environment.h>
 
 #include <Eigen/Core>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <map>
 
 using namespace jpathgen::environment;
 using Catch::Matchers::WithinRel;
 
-SCENARIO("MVBivarGaussians can be instatiated", "[MVBG]"){
-  GIVEN("Using a std vector with pair"){
-    STLMUS mus = {{0,0}};
-    STLCOVS covs = {{1,0},{0,1}};
+enum METHOD{STL, EIGEN};
 
-    WHEN("One mode is added"){
-      THEN("It works without error"){
+std::map<int, std::string> METHOD2STRING = {
+  {METHOD::STL, "STL"},
+  {METHOD::EIGEN, "EIGEN"},
+};
 
-        MultiModalBivariateGaussian mmbg(mus, covs);
-        REQUIRE(mmbg.length() == 1);
-      }
-      THEN("The value is 1/ ( 2 PI )")
+MultiModalBivariateGaussian create_unit_mmbg_using_stl(int n_modes){
+  STLMUS mus;
+  STLCOVS  covs;
+  for (int i=0; i<n_modes;i++){
+    mus.insert(mus.end(),1,{0,0});
+    covs.insert(covs.end(), 1, {1,0});
+    covs.insert(covs.end(), 1, {0,1});
+  }
+  MultiModalBivariateGaussian mmbg(mus, covs);
+  return mmbg;
+}
+
+MultiModalBivariateGaussian create_unit_mmbg_using_eigen(int n_modes){
+
+  MUS mus = Eigen::Matrix<double, -1, 2>::Zero(n_modes,2);
+  COV cov = COV::Identity();
+  COVS covs = Eigen::Matrix<double, -1, 2>::Zero(n_modes*2, 2);
+
+  for (int i = 0; i < n_modes; ++i)
+  {
+    covs.block<2,2>(i*2, 0) = cov;
+  }
+  MultiModalBivariateGaussian mmbg(mus, covs);
+  return mmbg;
+}
+
+MultiModalBivariateGaussian create_unit_mmbg(int n_modes, METHOD method){
+  switch (method)
+  {
+    case METHOD::STL:
+      return create_unit_mmbg_using_stl(n_modes);
+    case METHOD::EIGEN:
+      return create_unit_mmbg_using_eigen(n_modes);
+  }
+}
+
+
+SCENARIO("MVBivarGaussians can be instantiated", "[MVBG]")
+{
+  int N = GENERATE(1,2,5,10);
+  METHOD method = GENERATE(METHOD::STL, METHOD::EIGEN);
+  MultiModalBivariateGaussian mmbg = create_unit_mmbg(N, method);
+  DYNAMIC_SECTION("With " << METHOD2STRING[method])
+  {
+    THEN("The length is N")
+    {
+      REQUIRE(mmbg.length() == N);
+    }
+    THEN("The value at (0,0) is 1/ ( 2 PI )")
+    {
+      REQUIRE_THAT(mmbg(0, 0), WithinRel(1 / (2 * M_PI)));
+    }
+    THEN("It is at a maxima at (0,0)")
+    {
+      double const exp = 1 / (2 * M_PI);
+      for (double xi = -1; xi <= 1; xi += 0.5)
       {
-        MultiModalBivariateGaussian mmbg(mus, covs);
-        double act = mmbg(0,0);
-        REQUIRE_THAT(act, WithinRel(1/(2*M_PI)));
-      }
-    }
-  }
-
-  GIVEN("1 mode at (0,0)"){
-    MU mu = MU::Zero();
-    COV cov = COV::Identity();
-
-    REQUIRE( mu.sum() == 0);
-    REQUIRE( cov.sum() == 2);
-
-    MultiModalBivariateGaussian mmbg(mu, cov);
-
-    WHEN("The length is evaluated"){
-      THEN("The value is 1"){
-        REQUIRE(mmbg.length() == 1);
-      }
-    }
-
-    WHEN("MVBG is evaluated at (0,0)"){
-      double const exp = 1/(2*M_PI);
-
-      THEN("The value is 1/ ( 2 PI )"){
-          double act = mmbg(0,0);
-
-       REQUIRE_THAT(act, WithinRel(exp));
-
-      }
-      THEN("It is at a maxima "){
-       for(double xi=-1;xi<=1;xi+=0.5)
-       {
-         for(double yi=-1;yi<=1;yi+=0.5)
-         {
-           if (xi==0 && yi==0){
-             continue;
-           }
-           CHECK(mmbg(xi,yi) < exp);
-         }
-       }
-      }
-    }
-  }
-  GIVEN("4 modes at (0,0)"){
-    MUS mus = Eigen::Matrix<double, 4, 2>::Zero();
-    COV cov = COV::Identity();
-    COVS covs = (Eigen::Matrix<double, 8,2>() << cov,cov,cov,cov).finished();
-
-    REQUIRE( mus.sum() == 0);
-    REQUIRE( covs.sum() == 8);
-
-    MultiModalBivariateGaussian mmbg(mus, covs);
-
-    WHEN("The length is evaluated"){
-      THEN("The value is 4"){
-        REQUIRE(mmbg.length() == 4);
-      }
-    }
-
-    WHEN("MVBG is evaluated at (0,0)"){
-      double const exp = 1/(2*M_PI);
-
-      THEN("The value is 1 / ( 2 PI )"){
-        double act = mmbg(0,0);
-
-        REQUIRE_THAT(act, WithinRel(exp));
-
-      }
-      THEN("It is at a maxima "){
-        for(double xi=-1;xi<=1;xi+=0.5)
+        for (double yi = -1; yi <= 1; yi += 0.5)
         {
-          for(double yi=-1;yi<=1;yi+=0.5)
+          if (xi == 0 && yi == 0)
           {
-            if (xi==0 && yi==0){
-              continue;
-            }
-            CHECK(mmbg(xi,yi) < exp);
+            continue;
           }
+          CHECK(mmbg(xi, yi) < exp);
         }
       }
     }
   }
 }
-
 
 SCENARIO("MVBivarGaussians throws expected exceptions", "[MVBG]"){
   GIVEN("Incompatible mus and covs shapes are passed"){
--- test/src/integration_test.cpp
+++ test/src/integration_test.cpp
@@ -1,68 +1,80 @@
 // #  Copyright 2023 Jan-Hendrik Ewers
 // #  SPDX-License-Identifier: GPL-3.0-only
-#include <catch2/catch_test_macros.hpp>
-#include <catch2/matchers/catch_matchers_floating_point.hpp>
-#include <jpathgen/integration.h>
 #include <jpathgen/environment.h>
 #include <jpathgen/function.h>
-#include <jpathgen/geometry.h>
+#include <jpathgen/integration.h>
 
 #include <Eigen/Core>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <map>
 
 using namespace jpathgen::integration;
 using namespace jpathgen::function;
 using namespace jpathgen::environment;
 using namespace jpathgen::geometry;
+using Catch::Matchers::WithinRel;
 
 double fn1(double a, double b){
   return 1;
 }
 
+MultiModalBivariateGaussian generate_mmbg(int n_modes=1){
+  MUS mus = Eigen::Matrix<double, -1, 2>::Zero(n_modes,2);
+  COV cov = COV::Identity();
+  COVS covs = Eigen::Matrix<double, -1, 2>::Zero(n_modes*2, 2);
+
+  for (int i = 0; i < n_modes; ++i)
+  {
+    covs.block<2,2>(i*2, 0) = cov;
+  }
+  MultiModalBivariateGaussian mmbg(mus, covs);
+  return mmbg;
+}
+
 SCENARIO("The integration function is called with the various allowed types", "[integration]"){
-  GIVEN("A function::Function"){
+  SECTION("A function::Function"){
+    auto fn = fn1;
     WHEN("And a Eigen::Matrix of coords is passed"){
       EigenCoords coords = Eigen::Matrix<double, 10,2>::Random();
       THEN("A double is returned without error"){
-        REQUIRE(integrate_over_buffered_line(fn1, coords) > -INFINITY);
+        REQUIRE(integrate_over_buffered_line(fn, coords) > -INFINITY);
       }
     }
     WHEN("And a vector of coords is passed"){
       STLCoords coords = {{0,0},{0,1},{10,10}};
       THEN("A double is returned without error"){
-        REQUIRE(integrate_over_buffered_line(fn1, coords) > -INFINITY);
+        REQUIRE(integrate_over_buffered_line(fn, coords) > -INFINITY);
       }
     }
-    WHEN("A rectangle is passed"){
-      THEN("A double is returned without error"){
-        REQUIRE(integrate_over_rect(fn1, 0,10,0,10) > 0 );
-      }
-
-      THEN("The area of the bounds is returned when the function always returns 1"){
-        double act = integrate_over_rect(fn1, 0,10,0,10);
-        REQUIRE(act == 100);
+    WHEN("And a rectangle is passed"){
+      double act = integrate_over_rect(fn, 0,10,0,10);
+      THEN("The value is as expected"){
+        REQUIRE(act == 10*10);
       }
     }
   }
-  GIVEN("A MultiModalBivariateGaussian"){
-    MU mu = MU::Zero();
-    COV cov = COV::Identity();
-    MultiModalBivariateGaussian mmbg(mu, cov);
-
+  SECTION("A MultiModalBivariateGaussian"){
+    int n_modes = GENERATE(1,10,100);
+    auto fn = generate_mmbg(n_modes);
     WHEN("And a Eigen::Matrix of coords is passed"){
-      EigenCoords coords = Eigen::Matrix<double, 10,2>::Random();
+      int n_coords = GENERATE(2,50);
+      EigenCoords coords = Eigen::Matrix<double, -1,2>::Random(n_coords,2);
       THEN("A double is returned without error"){
-        REQUIRE(integrate_over_buffered_line(mmbg, coords) > -INFINITY);
+        REQUIRE(integrate_over_buffered_line(fn, coords) > -INFINITY);
       }
     }
     WHEN("And a vector of coords is passed"){
       STLCoords coords = {{0,0},{0,1},{10,10}};
       THEN("A double is returned without error"){
-        REQUIRE(integrate_over_buffered_line(mmbg, coords) > -INFINITY);
+        REQUIRE(integrate_over_buffered_line(fn, coords) > -INFINITY);
       }
     }
-    WHEN("A rectangle is passed"){
-      THEN("A double is returned without error"){
-        REQUIRE(integrate_over_rect(mmbg, 0,10,0,10) > 0 );
+    WHEN("And a rectangle is passed"){
+      double act = integrate_over_rect(fn, 0,10,0,10);
+      THEN("The value is as expected"){
+        REQUIRE_THAT(act, WithinRel(0.25,1e-6));
       }
     }
   }
