52e5930fad8e6c851bf4afcc96bbad1035440b9e
iwishiwasaneagle <jh.ewers@gmail.com>
2023-09-25 18:35:38 +0100

    fix: Return to old method as converting mmbg to function was messing with test coverage

--- test/src/integration_test.cpp
+++ test/src/integration_test.cpp
@@ -33,27 +33,9 @@ MultiModalBivariateGaussian generate_mmbg(int n_modes=1){
   return mmbg;
 }
 
-enum CALLABLE{FUNCTION, MMBG};
-std::map<int, std::string> CALLABLE2STRING = {
-  {CALLABLE::FUNCTION, "function::Function"},
-  {CALLABLE::MMBG, "MultiModalBivariateGaussian"},
-};
-
 SCENARIO("The integration function is called with the various allowed types", "[integration]"){
-  int n_modes = GENERATE(1, 5, 10);
-  CALLABLE method = GENERATE(CALLABLE::FUNCTION, CALLABLE::MMBG);
-  Function fn;
-  switch (method)
-  {
-    case CALLABLE::FUNCTION:
-      fn = fn1;
-      break;
-    case CALLABLE::MMBG:
-      fn = generate_mmbg(n_modes);
-      break;
-  }
-
-  DYNAMIC_SECTION("A " << CALLABLE2STRING[method]){
+  SECTION("A function::Function"){
+    auto fn = fn1;
     WHEN("And a Eigen::Matrix of coords is passed"){
       EigenCoords coords = Eigen::Matrix<double, 10,2>::Random();
       THEN("A double is returned without error"){
@@ -68,19 +50,31 @@ SCENARIO("The integration function is called with the various allowed types", "[
     }
     WHEN("And a rectangle is passed"){
       double act = integrate_over_rect(fn, 0,10,0,10);
-      THEN("A non-zero value is returned"){
-        REQUIRE(act > 0 );
-      }
       THEN("The value is as expected"){
-        switch (method)
-        {
-          case CALLABLE::FUNCTION:
-            REQUIRE(act == 10*10);
-            break;
-          case CALLABLE::MMBG:
-            REQUIRE_THAT(0.25, WithinRel(act, 1e-6));
-            break;
-        }
+        REQUIRE(act == 10*10);
+      }
+    }
+  }
+  SECTION("A MultiModalBivariateGaussian"){
+    int n_modes = GENERATE(1,10,100);
+    auto fn = generate_mmbg(n_modes);
+    WHEN("And a Eigen::Matrix of coords is passed"){
+      int n_coords = GENERATE(2,50);
+      EigenCoords coords = Eigen::Matrix<double, -1,2>::Random(n_coords,2);
+      THEN("A double is returned without error"){
+        REQUIRE(integrate_over_buffered_line(fn, coords) > -INFINITY);
+      }
+    }
+    WHEN("And a vector of coords is passed"){
+      STLCoords coords = {{0,0},{0,1},{10,10}};
+      THEN("A double is returned without error"){
+        REQUIRE(integrate_over_buffered_line(fn, coords) > -INFINITY);
+      }
+    }
+    WHEN("And a rectangle is passed"){
+      double act = integrate_over_rect(fn, 0,10,0,10);
+      THEN("The value is as expected"){
+        REQUIRE_THAT(act, WithinRel(0.25,1e-6));
       }
     }
   }
