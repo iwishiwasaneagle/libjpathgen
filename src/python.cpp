07be4f7d91186bbdb20e0d6f13d11d1676f12591
iwishiwasaneagle <me@janhendrikewers.uk>
2024-03-26 11:49:52 +0000

    doc: Ensure pybind11 wrappings have appropriate variable names that show up in python

--- src/python.cpp
+++ src/python.cpp
@@ -16,17 +16,23 @@ using namespace jpathgen::integration;
 using namespace jpathgen::function;
 using namespace jpathgen::environment;
 using namespace jpathgen::geometry;
+using namespace py::literals;
 
 PYBIND11_MODULE(_core, m)
 {
   m.doc() = "A C++ library to speed up jpathgen computations";
 
   py::class_<MultiModalBivariateGaussian>(m, "MultiModalBivariateGaussian")
-      .def(py::init<Eigen::Ref<MUS>, Eigen::Ref<COVS>>())
-      .def(py::init<STLMUS, STLCOVS>())
+      .def(py::init<Eigen::Ref<MUS>, Eigen::Ref<COVS>>(), "mus"_a, "covs"_a)
+      .def(py::init<STLMUS, STLCOVS>(), "mus"_a, "covs"_a)
       .def("__len__", [](MultiModalBivariateGaussian& mmbg) { return mmbg.length(); })
-      .def("__call__", [](MultiModalBivariateGaussian& mmbg, double x, double y) { return mmbg(x, y); })
-      .def("__call__", py::vectorize([](MultiModalBivariateGaussian& mmbg, double x, double y) { return mmbg(x, y); }))
+      .def(
+          "__call__", [](MultiModalBivariateGaussian& mmbg, double x, double y) { return mmbg(x, y); }, "x"_a, "y"_a)
+      .def(
+          "__call__",
+          py::vectorize([](MultiModalBivariateGaussian& mmbg, double x, double y) { return mmbg(x, y); }),
+          "x"_a,
+          "y"_a)
       .def(
           "__repr__",
           [](MultiModalBivariateGaussian& mmbg)
@@ -38,10 +44,20 @@ PYBIND11_MODULE(_core, m)
       .def_property_readonly("_mus", &MultiModalBivariateGaussian::getMus)
       .def_property_readonly("_covs", &MultiModalBivariateGaussian::getCovs);
 
-  py::class_<Args>(m, "Args").def(py::init<double>()).def_property_readonly("buffer_radius_m", &Args::get_buffer_radius_m);
+  py::class_<Args>(m, "Args")
+      .def(py::init<double>(), "buffer_radius_m"_a)
+      .def_property_readonly("buffer_radius_m", &Args::get_buffer_radius_m);
 
   py::class_<DiscreteArgs, Args>(m, "DiscreteArgs")
-      .def(py::init<double, int, int, double, double, double, double>())
+      .def(
+          py::init<double, int, int, double, double, double, double>(),
+          "buffer_radius_m"_a,
+          "N"_a,
+          "M"_a,
+          "minx"_a,
+          "maxx"_a,
+          "minx"_a,
+          "maxx"_a)
       .def_property_readonly("N", &DiscreteArgs::get_N)
       .def_property_readonly("M", &DiscreteArgs::get_M)
       .def_property_readonly("minx", &DiscreteArgs::get_minx)
@@ -52,96 +68,188 @@ PYBIND11_MODULE(_core, m)
   py::class_<ContinuousArgs, Args>(m, "ContinuousArgs")
       .def(
           py::init<double, double, double, unsigned long>(),
-          py::arg("buffer_radius_m"),
-          py::arg("abs_err_req") = 0.0,
-          py::arg("rel_err_req") = 0.05,
-          py::arg("max_eval") = 100000)
+          "buffer_radius_m"_a,
+          "abs_err_req"_a = 0.0,
+          "rel_err_req"_a = 0.05,
+          "max_eval"_a = 100000)
       .def_property_readonly("abs_err_req", &ContinuousArgs::get_abs_err_req)
       .def_property_readonly("rel_err_req", &ContinuousArgs::get_rel_err_req)
       .def_property_readonly("max_eval", &ContinuousArgs::get_max_eval);
 
-  m.def(
-      "continuous_integration_over_polygon",
-      static_cast<double (*)(Function, STLCoords, ContinuousArgs*)>(&continuous_integration_over_polygon));
-  m.def(
-      "continuous_integration_over_polygon",
-      static_cast<double (*)(MultiModalBivariateGaussian, STLCoords, ContinuousArgs*)>(
-          &continuous_integration_over_polygon));
-  m.def(
-      "discrete_integration_over_polygon",
-      static_cast<double (*)(Function, STLCoords, DiscreteArgs*)>(&discrete_integration_over_polygon));
-  m.def(
-      "discrete_integration_over_polygon",
-      static_cast<double (*)(MultiModalBivariateGaussian, STLCoords, DiscreteArgs*)>(&discrete_integration_over_polygon));
+  auto F = "f"_a;
+  auto ARGS = "args"_a;
 
+  auto POLYGON = "polygon"_a;
+  m.def(
+      "continuous_integration_over_polygon",
+      static_cast<double (*)(Function, STLCoords, ContinuousArgs*)>(&continuous_integration_over_polygon),
+      F,
+      POLYGON,
+      ARGS);
+  m.def(
+      "continuous_integration_over_polygon",
+      static_cast<double (*)(MultiModalBivariateGaussian, STLCoords, ContinuousArgs*)>(&continuous_integration_over_polygon),
+      F,
+      POLYGON,
+      ARGS);
+  m.def(
+      "discrete_integration_over_polygon",
+      static_cast<double (*)(Function, STLCoords, DiscreteArgs*)>(&discrete_integration_over_polygon),
+      F,
+      POLYGON,
+      ARGS);
+  m.def(
+      "discrete_integration_over_polygon",
+      static_cast<double (*)(MultiModalBivariateGaussian, STLCoords, DiscreteArgs*)>(&discrete_integration_over_polygon),
+      F,
+      POLYGON,
+      ARGS);
+
+  auto LEFT = "left"_a;
+  auto RIGHT = "right"_a;
+  auto BOTTOM = "bottom"_a;
+  auto TOP = "top"_a;
   m.def(
       "continuous_integration_over_rectangle",
       static_cast<double (*)(Function, double, double, double, double, ContinuousArgs*)>(
-          &continuous_integration_over_rectangle));
+          &continuous_integration_over_rectangle),
+      F,
+      LEFT,
+      RIGHT,
+      BOTTOM,
+      TOP,
+      ARGS);
   m.def(
       "continuous_integration_over_rectangle",
       static_cast<double (*)(MultiModalBivariateGaussian, double, double, double, double, ContinuousArgs*)>(
-          &continuous_integration_over_rectangle));
+          &continuous_integration_over_rectangle),
+      F,
+      LEFT,
+      RIGHT,
+      BOTTOM,
+      TOP,
+      ARGS);
   m.def(
       "discrete_integration_over_rectangle",
-      static_cast<double (*)(Function, double, double, double, double, DiscreteArgs*)>(
-          &discrete_integration_over_rectangle));
+      static_cast<double (*)(Function, double, double, double, double, DiscreteArgs*)>(&discrete_integration_over_rectangle),
+      F,
+      LEFT,
+      RIGHT,
+      BOTTOM,
+      TOP,
+      ARGS);
   m.def(
       "discrete_integration_over_rectangle",
       static_cast<double (*)(MultiModalBivariateGaussian, double, double, double, double, DiscreteArgs*)>(
-          &discrete_integration_over_rectangle));
+          &discrete_integration_over_rectangle),
+      F,
+      LEFT,
+      RIGHT,
+      BOTTOM,
+      TOP,
+      ARGS);
 
+  auto COORDS = "coords"_a;
   m.def(
       "continuous_integration_over_path",
-      static_cast<double (*)(Function, STLCoords, ContinuousArgs*)>(&continuous_integration_over_path));
+      static_cast<double (*)(Function, STLCoords, ContinuousArgs*)>(&continuous_integration_over_path),
+      F,
+      COORDS,
+      ARGS);
   m.def(
       "continuous_integration_over_path",
-      static_cast<double (*)(Function, EigenCoords, ContinuousArgs*)>(&continuous_integration_over_path));
+      static_cast<double (*)(Function, EigenCoords, ContinuousArgs*)>(&continuous_integration_over_path),
+      F,
+      COORDS,
+      ARGS);
   m.def(
       "continuous_integration_over_path",
-      static_cast<double (*)(MultiModalBivariateGaussian, STLCoords, ContinuousArgs*)>(&continuous_integration_over_path));
+      static_cast<double (*)(MultiModalBivariateGaussian, STLCoords, ContinuousArgs*)>(&continuous_integration_over_path),
+      F,
+      COORDS,
+      ARGS);
   m.def(
       "continuous_integration_over_path",
-      static_cast<double (*)(MultiModalBivariateGaussian, EigenCoords, ContinuousArgs*)>(&continuous_integration_over_path));
+      static_cast<double (*)(MultiModalBivariateGaussian, EigenCoords, ContinuousArgs*)>(&continuous_integration_over_path),
+      F,
+      COORDS,
+      ARGS);
   m.def(
       "discrete_integration_over_path",
-      static_cast<double (*)(Function, STLCoords, DiscreteArgs*)>(&discrete_integration_over_path));
+      static_cast<double (*)(Function, STLCoords, DiscreteArgs*)>(&discrete_integration_over_path),
+      F,
+      COORDS,
+      ARGS);
   m.def(
       "discrete_integration_over_path",
-      static_cast<double (*)(Function, EigenCoords, DiscreteArgs*)>(&discrete_integration_over_path));
+      static_cast<double (*)(Function, EigenCoords, DiscreteArgs*)>(&discrete_integration_over_path),
+      F,
+      COORDS,
+      ARGS);
   m.def(
       "discrete_integration_over_path",
-      static_cast<double (*)(MultiModalBivariateGaussian, STLCoords, DiscreteArgs*)>(&discrete_integration_over_path));
+      static_cast<double (*)(MultiModalBivariateGaussian, STLCoords, DiscreteArgs*)>(&discrete_integration_over_path),
+      F,
+      COORDS,
+      ARGS);
   m.def(
       "discrete_integration_over_path",
-      static_cast<double (*)(MultiModalBivariateGaussian, EigenCoords, DiscreteArgs*)>(&discrete_integration_over_path));
+      static_cast<double (*)(MultiModalBivariateGaussian, EigenCoords, DiscreteArgs*)>(&discrete_integration_over_path),
+      F,
+      COORDS,
+      ARGS);
 
+  auto COORDS_VEC = "coords_vec"_a;
   m.def(
       "continuous_integration_over_paths",
-      static_cast<double (*)(Function, std::vector<STLCoords>, ContinuousArgs*)>(&continuous_integration_over_paths));
+      static_cast<double (*)(Function, std::vector<STLCoords>, ContinuousArgs*)>(&continuous_integration_over_paths),
+      F,
+      COORDS_VEC,
+      ARGS);
   m.def(
       "continuous_integration_over_paths",
-      static_cast<double (*)(Function, std::vector<EigenCoords>, ContinuousArgs*)>(&continuous_integration_over_paths));
+      static_cast<double (*)(Function, std::vector<EigenCoords>, ContinuousArgs*)>(&continuous_integration_over_paths),
+      F,
+      COORDS_VEC,
+      ARGS);
   m.def(
       "continuous_integration_over_paths",
       static_cast<double (*)(MultiModalBivariateGaussian, std::vector<STLCoords>, ContinuousArgs*)>(
-          &continuous_integration_over_paths));
+          &continuous_integration_over_paths),
+      F,
+      COORDS_VEC,
+      ARGS);
   m.def(
       "continuous_integration_over_paths",
       static_cast<double (*)(MultiModalBivariateGaussian, std::vector<EigenCoords>, ContinuousArgs*)>(
-          &continuous_integration_over_paths));
+          &continuous_integration_over_paths),
+      F,
+      COORDS_VEC,
+      ARGS);
   m.def(
       "discrete_integration_over_paths",
-      static_cast<double (*)(Function, std::vector<STLCoords>, DiscreteArgs*)>(&discrete_integration_over_paths));
+      static_cast<double (*)(Function, std::vector<STLCoords>, DiscreteArgs*)>(&discrete_integration_over_paths),
+      F,
+      COORDS_VEC,
+      ARGS);
   m.def(
       "discrete_integration_over_paths",
-      static_cast<double (*)(Function, std::vector<EigenCoords>, DiscreteArgs*)>(&discrete_integration_over_paths));
+      static_cast<double (*)(Function, std::vector<EigenCoords>, DiscreteArgs*)>(&discrete_integration_over_paths),
+      F,
+      COORDS_VEC,
+      ARGS);
   m.def(
       "discrete_integration_over_paths",
       static_cast<double (*)(MultiModalBivariateGaussian, std::vector<STLCoords>, DiscreteArgs*)>(
-          &discrete_integration_over_paths));
+          &discrete_integration_over_paths),
+      F,
+      COORDS_VEC,
+      ARGS);
   m.def(
       "discrete_integration_over_paths",
       static_cast<double (*)(MultiModalBivariateGaussian, std::vector<EigenCoords>, DiscreteArgs*)>(
-          &discrete_integration_over_paths));
+          &discrete_integration_over_paths),
+      F,
+      COORDS_VEC,
+      ARGS);
 }
